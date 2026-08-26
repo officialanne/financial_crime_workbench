c7674d4c5b2ed14e28ea63f6a337859ac5abd26e
officialanne <anneasare7@gmail.com>
2026-08-27 00:51:05 +0100

    setting up the executive dashboard as the default starting page

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
 # Define pages
+dashboard_page = st.Page("pages/dashboard.py", title="Executive Dashboard", default=True)
 txn_risk_page = st.Page("pages/transactions.py", title="Transactions Risk Explorer")
 
+
 # Setup navigation
-pg = st.navigation([txn_risk_page])
+pg = st.navigation([dashboard_page, txn_risk_page])
 
 # Run the selected page
 pg.run()
